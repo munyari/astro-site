82209820daf92c4de1b5ec5bac1c6ca288c19a78
Panashe M. Fundira <5841072+munyari@users.noreply.github.com>
2025-02-21 14:56:37 -0500

    Factor Section component

--- src/react/components/CalendarBooker.tsx
+++ src/react/components/CalendarBooker.tsx
@@ -1,20 +1,23 @@
 import Cal, { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
+const layout = "month_view";
+const namespace = "discovery";
+
 export default function CalendarBooker() {
   useEffect(() => {
     (async function () {
-      const cal = await getCalApi({ namespace: "discovery" });
-      cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+      const cal = await getCalApi({ namespace });
+      cal("ui", { hideEventTypeDetails: false, layout });
     })();
   }, []);
 
   return (
     <Cal
-      namespace="discovery"
+      namespace={namespace}
       calLink="panashe/discovery"
       style={{ width: "100%", height: "50%", overflow: "scroll" }}
-      config={{ layout: "month_view" }}
+      config={{ layout }}
     />
   );
 }
